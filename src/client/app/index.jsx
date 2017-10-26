295bedae4b7c88b2add15b82f2ab9d98b9c8c211
peteral <ladislav.petera@yahoo.de>
2017-10-26 15:50:16 +0200

    Set up build system

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import AwesomeComponent from './AwesomeComponent.jsx'
 
 class App extends React.Component {
     render () {
-        return <p> Hello react...</p>
+        return  (
+            <div>
+                <AwesomeComponent/>
+            </div>
+        )
     }
 }
 
